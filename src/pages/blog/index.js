fb7883be0f53aeba3b286cb631c4fe034dfc91de
anhilmy <hilmyahmadnaufal@gmail.com>
2022-03-01 19:26:25 +0700

    finished part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
@@ -9,17 +9,15 @@ const BlogPage = ({ data }) => {
             {
                 data.allMdx.nodes.map((node) => (
                     <article key={node.id}>
-                        <h2>{node.frontmatter.title}</h2>
+                        <h2><Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link> </h2>
                         <p>Posted: {node.frontmatter.date}</p>
-                        <MDXRenderer>
-                            {node.body}
-                        </MDXRenderer>
                     </article>
                 ))
             }
         </Layout>
     )
 }
+
 
 export default BlogPage
 
@@ -32,7 +30,7 @@ export const query = graphql`
                 title
               }
               id
-              body
+              slug
             }
         }
     }    
